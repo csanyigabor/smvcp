3674ba303fbbec85569bdec0724e37507c0bb3d7
Gabor Csanyi <csanyi.gabor@webndev.hu>
2015-01-03 12:01:18 +0100

    adding empty controller for subscribers

---
+++ src/Controller/SubscriberController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WND\SMVCP\Controller;
+
+use WND\SMVCF\Http\Controller;
+
+class SubscriberController extends Controller
+{
+    public function listAction()
+    {}
+
+    public function createAction()
+    {}
+}
