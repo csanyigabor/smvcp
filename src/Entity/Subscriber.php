9fde6959e43f8a2dcd39965a87abe6fdd24a5f68
Gabor Csanyi <csanyi.gabor@webndev.hu>
2015-01-04 12:50:06 +0100

    createing Subscriber entity

---
+++ src/Entity/Subscriber.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace WND\SMVCP\Entity;
+
+class Subscriber
+{
+    /**
+     * @type int
+     */
+    protected $id;
+
+    /**
+     * @type string
+     */
+    protected $name;
+
+    /**
+     * @type string
+     */
+    protected $email;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return \WND\SMVCP\Entity\Subscriber
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param $email
+     *
+     * @return \WND\SMVCP\Entity\Subscriber
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
