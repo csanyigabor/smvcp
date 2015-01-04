09cccfd816896790b022a0641d2ec3429ca1d030
Gabor Csanyi <csanyi.gabor@webndev.hu>
2015-01-04 13:01:57 +0100

    tie up doctrine and controller

--- src/Controller/SubscriberController.php
+++ src/Controller/SubscriberController.php
@@ -5,23 +5,41 @@ namespace WND\SMVCP\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use WND\SMVCF\Http\Controller;
+use WND\SMVCP\Entity\Subscriber;
 use WND\SMVCP\Form\CreateSubscriberType;
 
 class SubscriberController extends Controller
 {
     public function listAction()
     {
-        return $this->render('View/Subscriber/index.php');
+        $em = $this->get('doctrine')->getManager();
+        $subscribers = $em
+            ->getRepository('WND\SMVCP\Entity\Subscriber')
+            ->getLatest(10);
+
+        return $this->render(
+            'View/Subscriber/index.php',
+            [
+                'subscribers' => $subscribers,
+            ]
+        );
     }
 
     public function createAction()
     {
-        $request = $this->get('request_stack')->getCurrentRequest();
+        $em = $this->get('doctrine')->getManager();
 
-        $form = $this->createForm(new CreateSubscriberType());
+        $subscriber = new Subscriber;
+
+        $form = $this->createForm(new CreateSubscriberType(), $subscriber);
+
+        $request = $this->get('request_stack')->getCurrentRequest();
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+            $em->persist($subscriber);
+            $em->flush();
+
             return $this->redirect('subscriber_list');
         }
 
--- src/View/Subscriber/index.php
+++ src/View/Subscriber/index.php
@@ -6,5 +6,27 @@
         <div>
             <a href="<?php echo $router->generate('subscriber_create') ?>">Create new subscriber</a>
         </div>
-    </header>
+        <table>
+            <thead>
+                <tr>Name</tr>
+                <tr>Email</tr>
+            </thead>
+            <tbody>
+                <?php if (count($subscribers) > 0): ?>
+                    <?php foreach ($subscribers as $subscriber): ?>
+                        <tr>
+                            <td><?php echo $view->escape($subscriber->getName()) ?></td>
+                            <td><?php echo $view->escape($subscriber->getEmail()) ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="100%">
+                            <em>No results found</em>
+                        </td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+</header>
 <?php $view['slots']->stop() ?>
